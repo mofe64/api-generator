9dc342b0ea7678fb667bff00618dd301d2673026
mofe <ogunbiyioladapo33@gmail.com>
2023-12-03 11:01:37 +0000

    index procedure for express/mongodb setup

---
+++ procedures/js/express-mongodb/index.js
@@ -0,0 +1,14 @@
+import sampleApi from "../../../sampleApi.json" assert { type: "json" };
+// console.log(sampleApi);
+const dataModels = sampleApi.models;
+console.log(dataModels);
+
+//extract models
+
+//check for model relationships
+
+// validate model relationships
+
+// prepare template context
+
+// generate source code based off template and context
